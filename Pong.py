fdc21081898ff3e7ebc3b29ba60d154cd85c4868
Peter Glenn <peter.glenn.17@cnu.edu>
2019-06-03 23:09:15 -0400

    Added two rectangles representing the two players

--- Pong.py
+++ Pong.py
@@ -1,21 +1,32 @@
 
 
-# Pong!!
+# Pong - Python Edition
+# @author Peter Glenn
+# @version 6.3.2019
+
+# Game constants
+FRAMERATE = 60
+SCREEN_X = 600
+SCREEN_Y = 400
 
 # Setting up pygame
 import pygame
 pygame.init()
-size = (700, 500)
+size = (SCREEN_X, SCREEN_Y)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("test name")
 clock = pygame.time.Clock()
 
+# Player locations
+player_one_location = SCREEN_Y / 2 - 15
+player_two_location = SCREEN_Y / 2 - 15
+
 # Defining colors
 BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
 
 # Main Game Loop
 running = True
-x = 0
 while running:
 
 	# Test for closed game window
@@ -24,15 +35,20 @@ while running:
 			running = False
 
 	# Game logic
-	x += 1
-	print("test", x)
 
-	# Draw screen
+
+	# Draw Screen 
 	screen.fill(BLACK)
+	# Draw Player 1
+	pygame.draw.rect(screen, GREEN, [15, player_one_location, 15, 50], 0)
+	# Draw Player 2
+	pygame.draw.rect(screen, GREEN, [SCREEN_X - 30, player_two_location, 15, 50], 0)
+
+	# Update display
 	pygame.display.flip()
 
 	# Frames per second limit
-	clock.tick(60)
+	clock.tick(FRAMERATE)
 
 # Quit game
 pygame.quit()
